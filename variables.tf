d80eb47101c2b48a3c0bc4ed8a8d5631ba0eee86
Prodan Oleg <prodan.oleg.v@gmail.com>
2025-08-29 19:20:31 -0400

    tf update

---
+++ variables.tf
@@ -0,0 +1,25 @@
+# variables.tf
+
+variable "docker_image" {
+  description = "The full ECR path and name of the Docker image to deploy."
+  type        = string
+  # No default value is set because the pipeline must always provide this.
+}
+
+variable "image_tag" {
+  description = "The tag of the Docker image to deploy (e.g., the build number)."
+  type        = string
+  # No default value is set because the pipeline must always provide this.
+}
+
+variable "environment" {
+  description = "The deployment environment, used for naming and workspaces (e.g., 'staging' or 'production')."
+  type        = string
+  # No default value is set because the pipeline must always provide this.
+}
+
+variable "instance_count" {
+  description = "The number of container instances (tasks) to run."
+  type        = number
+  # No default value is set because the pipeline explicitly sets this for each environment.
+}
